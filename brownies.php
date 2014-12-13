6b233733d5b6aa21a30a0bbae6d44c99df641fd7
Philip Kahn <tigerhawkvok@gmail.com>
2014-12-12 20:06:23 -0800

    Update brownie recipe to what I'm currently using

--- brownies.php
+++ brownies.php
@@ -5,14 +5,15 @@ include('header.inc');
 
 <ul id='ingredients'>
   <!-- Ingredients in unordered list -->
-<li>1 stick butter (4 oz), softened</li>
+<li>2 stick butter (4 oz), softened</li>
 <li>1 cup packed brown sugar</li>
 <li>1 cup white sugar</li>
 <li>3 eggs</li>
-<li>2 teaspoons vanilla extract</li>
-<li>1 cup chocolate chips</li>
+<li>1 Tbsp vanilla extract</li>
+<li>2 Tbsp butter</li>
+<li>1.5 cup chocolate chips</li>
+<li>0.5 cup cream (milk will do in a pinch)</li>
 <li>2 cups flour</li>
-<li>1 cup chocolate pudding</li>
 <li>1/2 teaspoon salt</li>
 <li>1.5  teaspoon baking powder</li>
 
@@ -22,9 +23,11 @@ include('header.inc');
   <!-- Steps as ordered list elements -->
 
 <li>In a mixing bowl, cream butter and sugar. </li>
-<li>Add the pudding, then the eggs, one at a time, beating well after
+<li>Add the eggs, one at a time, beating well after
   each addition. </li>
-<li>Beat in vanilla. </li>
+<li>In a container to double boil, add the chocolate chips, vanilla, the 2 Tbsp butter, and 1/2 cup cream</li>
+<li>Mix in the double boiler until a smooth chocolatey sauce has formed.</li>
+<li>Add chocolate sauce to buttercream mix.</li>
 <li>Combine the flour, baking powder and salt; gradually add to creamed mixture.</li>
 <li>Melt chocolate chips in saucepan (nonstick) with a little bit of butter.  Beat into mixture.</li>
 <li>Spread into a greased 13-in. x 9-in. x 2-in. baking pan. Spread remaining batter evenly over top. Bake at 350 degrees F for 30-35 minutes or until brownies begin to pull away from sides of pan. Cool on a wire rack.</li>

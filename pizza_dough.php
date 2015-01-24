a4310e75cc5261f7dbfdfdaeb6e21881dbfc4739
Philip Kahn <tigerhawkvok@gmail.com>
2015-01-23 17:50:15 -0800

    Update pizza dough

--- pizza_dough.php
+++ pizza_dough.php
@@ -3,23 +3,23 @@ include('header.inc');
 ?>
 
 <ul>
-<li>1.3 tablespoons active dry yeast</li>
-<li>1-2 tablespoon white sugar</li>
+<li>1.3 tablespoons (4tsp) active dry yeast</li>
+<li>2 tablespoon white sugar</li>
 <li>2 tablespoons honey</li>
 <li>2 1/2 cups warm water (110 degrees F)</li>
 <li>2 tablespoons olive oil</li>
 <li>1 tablespoon salt</li>
-<li>2 cups whole wheat flour</li>
-<li>4 cups bread flour</li>
+<li>1 cups whole wheat flour</li>
+<li>5 cups bread flour</li>
 </ul>
 
 <ol class='process'>
 <li>In a large mixing bowl, dissolve yeast and sugar in the warm water. Let sit until creamy; about 10 minutes.</li>
 <li>Stir the olive oil and salt.</li>
-<li>Stir in the whole wheat flour and 2.5 cups of normal flour into the yeast mixture (so, the first 4.5 cups). Mix in the remaining flour, 1/2 cup at a time (thus, three times), stirring well after each addition. </li>
+<li>Stir in the whole wheat flour and 3 cups of normal flour into the yeast mixture (so, the first 4 cups). Mix in the remaining flour, 1/2 cup at a time (thus, four times), stirring well after each addition. </li>
 <li>When the dough has pulled together, turn it out onto a lightly floured surface and knead until smooth and elastic, about 8 minutes. </li>
 <li>Lightly oil a large mixing bowl, place the dough in the bowl and turn to coat with oil. Cover with a damp cloth and put in a warm place to rise until doubled in volume; about 1 hour.  Let it stay longer for fluffier dough.</li>
-<li>Deflate the dough and turn it out onto a lightly floured surface. Divide the dough into three equal pieces and form into rounds. Cover the rounds and let them rest for about 10 minutes. Preheat oven to 425-450<sup>o</sup>  F.</li>
+<li>Deflate the dough and turn it out onto a lightly floured surface. Divide the dough into three equal pieces and form into rounds. Cover the rounds and let them rest for about 10 minutes. Preheat oven to 450-500<sup>o</sup>  F.</li>
 <li>Use a rolling pin to roll the dough into the desired shape, cover it with your favorite toppings and bake for 10-20 minutes or until the crust and cheese are golden brown.</li>
 
 </ol>

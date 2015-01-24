88ef5f1e4cd3dc34a6c7fbbefa70ad0e86e9105a
cagliari2005 <cagliari2005@hotmail.com>
2015-01-24 10:46:48 -0800

    Create apple_cramble.php

---
+++ apple_cramble.php
@@ -0,0 +1,15 @@
+<?php $title="Apple Cramble";
+include("header.inc");?>
+<ul>
+<li>4 firm medium apples</li>
+<li>1 cup sugar</li>
+<li>1 cup all-purpose flour</li>
+<li>1 butter stick<li>
+<li>1 tablespoon ground cinnamon</li>
+</ul>
+<p class="process">Wash and core apples, cut into dices. Place in a baking dish.
+  Sprinkle the cinnamon over the apple. Mix the sugar and flour in a a bowl and add 
+  the melt butter. Rub in with fingertips until the topping comes together in moist 
+  clumps. Sprinkle the topping over the apple. Bake the crumble until apples are tender 
+  and the topping appears brown and crisp, about 45 minutes @400.</p>
+<?php include("footer.inc");?>

79104e063d6522ed13c08cb28a34c8dac11c2ff4
Philip Kahn <tigerhawkvok@gmail.com>
2015-12-21 00:28:31 -0800

    Add toffee

---
+++ toffee.php
@@ -0,0 +1,36 @@
+<?php
+// Save this file as name_of_recipe.php
+$title=ucwords(str_replace("_"," ",basename($_SERVER["REQUEST_URI"],'.php')));
+include('header.inc');
+
+?>
+
+<ul id='ingredients'>
+  <!-- Ingredients in unordered list -->
+  <li>2 cups refined granulated sugar</li>
+  <li>1 lbs / 454g unsalted butter</li>
+  <li>18oz - 24oz semisweet chocolate chips/baking wafers</li>
+  <li>1/4 tsp salt</li>
+  <li>2 cups blanched and peeled almonds (still moist; add 1/4 tsp water if dry )</li>
+</ul>
+<p>AKA, "Hannah Lyman's Grandma's Recipe"</p>
+<ol id='process'>
+  <!-- Steps as ordered list elements -->
+  <li>Prepare a deep cookie sheet with a Al foil and silicone liner on top of that.</li>
+  <li>Prepare the chocolate -- chunks should be small and therefore
+    spreadable.</li>
+  <li>Prepare high-heat ready utensils</li>
+  <li>Combine sugar and butter in md/lg saucepan on medium heat and gently stir still mixed.</li>
+  <li>Once mixed and melted together, add almonds. These will serve as your cooking timer, too, so it's important to be prompt!</li>
+  <li>Leave on medium heat (DO NOT REDUCE) and stir as infrequently as possible until it begins to thicken</li>
+  <li><strong>IMPORTANT:</strong> if the oil begins to separate, increase heat slightly (eg, from 5/10 -> 6-7/10). Do NOT stir faster unless the sugar has begun to candy! This will be obvious, as the sugar will start to be "tendrily", think, and stringy as you stir it. You may add a pinch more sugar to further stabilize the mixture.</li>
+  <li>The almonds will begin to cook and make crackling/popping noises. This is a good sign.</li>
+  <li>Toffee will begin to turn a darker brown, about that of golden brown brown sugar. Begin to stir faster, as fast as you dare (beware molten sugar/butter!) The mixture MUST stay mixed to "work".</li>
+  <li>When almonds are dark brown and buttercream mixture is golden/dark brown, and it is aromatic with almonds (Should be ~ 155&#176;C &#177; 4&#176;C, pour into the cookie sheet.</li>
+  <li>Sprinkle chocolate evenly over the top while still molten/bubbling. Chocolate will immediately begin to melt and penetrate the mixture. Spread out as evenly as possible while it does so.</li>
+  <li>Let stand 30-45 minutes before placing it into the fridge/freezer/sampling.</li>
+</ol>
+
+<?php
+include('footer.inc');
+?>

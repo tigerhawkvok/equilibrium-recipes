6a85a1a1199c5630860c7c13e41cc6c3bb18c5f2
Philip Kahn <tigerhawkvok@gmail.com>
2020-07-04 17:09:08 -0700

    Tiramisu recipe as from Shelby

---
+++ tiramisu.php
@@ -0,0 +1,61 @@
+<?php
+// Save this file as name_of_recipe.php
+$title=ucwords(str_replace("_"," ",basename($_SERVER["REQUEST_URI"],'.php')));
+include('header.inc');
+
+?>
+
+<ul id='ingredients'>
+  <!-- Ingredients in unordered list -->
+  <li>Coffee (enough for 300ml / 1.25 cups)</li>
+  <li>6 Tablespoons bourbon whiskey</li>
+  <li>40-45 ladyfingers (~1.5 packages)</li>
+  <li>16oz marscapone</li>
+  <li>0.25 cup dark spiced rum (eg, Kraken)</li>
+  <li>4 large eggs</li>
+  <li>1/2 cup baking sugar</li>
+  <li>2 cups heavy cream</li>
+  <li>1/2 teasppon vanilla extract</li>
+  <li>1/8 teaspoon salt</li>
+  <li>Dutch process cocoa powder</li>
+</ul>
+
+<p><strong>You should have ready...</strong></p>
+<ul id='equipment'>
+
+  <li>A 9x13 pan, prefereably deep. Don't have open handles; you will want to seal this moderately well at the end.</li>
+  <li>An aeropress or other way to make espresso or espresso-like coffee</li>
+  <li>A double-boiler setup</li>
+  <li>Hand whisk</li>
+  <li>Stand mixer or be prepared for lots of whisking</li>
+</ul>
+
+<ol id='process'>
+  <li>Heat water to 93C for the coffee, and ready your Aeropress. Put the plunger to the max draw.</li>
+  <li>Grind very fine grounds. Add 2 tablespoons to your Aeropress, and tamp down</li>
+  <li>Put the filter in the aeropress drain cap and rinse with hot water, then fill with the water. Stir, and wait 30 seconds.</li>
+  <li>Press out the coffee into a cup measure, and repeat until you have 1.25 cups of espresso</li>
+  <li>Whisk the espresso and bourbon together in a bowl or tupperware with a flat area big enough for the ladyfingers</li>
+  <li><strong>Quickly</strong> dip the first layer of ladyfingers in the mixture. More a dab on both sides than a soak. Create a solid bottom layer in the 9x13; you may need to trim a few ladyfingers.</li>
+  <li>Reserve the remainder of the espresso-whisky for the next layer, and set aside.</li>
+  <li>Separate the eggs</li>
+  <li>Whisk the marscapone and rum on medium-low until smooth in your stand mixer.</li>
+  <li>Set up the double boiler and get the water hot and reduce to medium-low; in your double boiler pan, put the egg yolks and 1/4 cup of the baking sugar (eg, half the total)</li>
+  <li>Heat the egg yolk-sugar mix and whisk until light and foamy (~5min)</li>
+  <li>Remove from heat and immediately pour into marscapone mix. Whisk on medium until well mixed.</li>
+  <li>Take stand mixture blend, move into a large bowl, and clean your stand mixer bowl and whisk.</li>
+  <li>Whisk the heavy cream and vanilla extract together on medium-high speed until medium peaks form, about 3-4 minutes. Medium peaks are between soft/loose peaks and stiff peaks– and are the perfect consistency for the tiramisu cream. With a spatula, fold the whipped cream into the mascarpone mixture.</li>
+  <li>Clean your stand mixer bowl and whisk again, thoroughly. Dry it.</li>
+  <li>Beat the egg whites and salt together on medium-high speed until foamy, about 1 minute. Increase to high speed and slowly pour in the remaining sugar (1/4 cup). Beat until stiff peaks form, about 4-5 minutes. (Do not over-beat as the egg whites will become dry.) </li>
+  <li>Fold in the marscapone mix</li>
+  <li>Spread half of the mascarpone cream evenly over bottom layer of ladyfingers.</li>
+  <li>Dip remaining ladyfingers into remaining espresso mixture and arrange one-by-one on top of the mascarpone layer. Gently press each down so they are nice and compact.</li>
+  <li>Spread remaining mascarpone mixture evenly on top</li>
+  <li>Chill uncovered 2-3 hours</li>
+  <li>Post-chill, sift Dutch cocoa powder until a dense layer is all over the top. You may serve now (I usually do)</li>
+  <li>Further chilling and storage should be done covered with plastic wrap.</li>
+</ol>
+
+<?php
+include('footer.inc');
+?>

bc07fe16bb4eb0a26cce48438d92dbfda9e50a4d
Philip Kahn <tigerhawkvok@gmail.com>
2021-01-06 21:17:27 -0800

    Cheesecake process update

--- cheesecake.php
+++ cheesecake.php
@@ -11,7 +11,7 @@ include('header.inc');
   <li>2 Tbsp butter</li>
   <li>4x8oz packs cream cheese</li>
   <li>1.5 cups sugar</li>
-  <li>3/4 cups milk (whole)</li>
+  <li>3/4 cups milk (whole; I like the texture from cream top)</li>
   <li>4 eggs</li>
   <li>1 cup sour cream</li>
   <li>1 Tbsp vanilla extract</li>
@@ -21,17 +21,17 @@ include('header.inc');
 
 <ol id='process'>
   <!-- Steps as ordered list elements -->
-  <li>Allow all ingredients to reach room temperature. This is very
-  important!</li>
+  <li><b>Allow all ingredients to reach room temperature. This is very
+  important!</b></li>
+  <li><b>ALL mixing should be done by hand or on the lowest setting of a stand mixer</b>. Do NOT overmix.</li>
   <li>Grease the sides of a springform pan with butter</li>
   <li>Mix crushed graham crackers and butter and press into the bottom
   of a springform pan to form a crust.</li>
-  <li>Preheat the oven to 350<sup>o</sup>F / 175<sup>o</sup>C</li>
-  <li>Mix cream cheese and sugar BY HAND until smooth. Do not
-  overmix.</li>
+  <li>Preheat the oven to 350<sup>o</sup>F / 175<sup>o</sup>C. On the rack below where you will place the cheesecake, put in a 9x13 filled with water during the preheat. (This will help prevent cracking).</li>
+  <li>Mix cream cheese and sugar  until smooth.</li>
   <li>Blend in milk, then mix in the eggs one at a time until
   incorporated. Mix in sour cream, vanilla, flour, and cream of tartar
-  until smooth, all BY HAND. Pour into crust.</li>
+  until smooth. Pour into crust.</li>
   <li>Bake in preheated oven for one hour.</li>
   <li>Fill a cookie sheet or shallow pan with some boiling water and
   place it on a shelf under the cake in the oven, then turn off the

360b37db97599f97305cc289f542e1a8c8492d22
Philip Kahn <tigerhawkvok@gmail.com>
2021-06-26 23:33:04 -0700

    Don't show some git files in the listing

--- beef_stroganoff.php
+++ beef_stroganoff.php
@@ -13,8 +13,8 @@ include('header.inc');
 <li>2 cups chicken broth</li>
 <li>~ 1/2 Tbsp mustard</li>
 <li>1.5 cups mushrooms (sliced)</li>
-<li>1/2 cup sour cream (full, or slightly more)</li>
-<li>~ 1/3 cup white wine</li>
+<li>1/2 cup sour cream</li>
+<li>~ 1/2 cup white wine</li>
 <li>2 lbs egg noodles</li>
 </ul>
 
@@ -23,7 +23,7 @@ include('header.inc');
 <ol>
 <li>Cut beef into small pieces (think d6 to d20 sized) and season with salt and pepper</li>
 <li>In skillet, melt the butter and brown the beef.  Push them off to one side.  Add onions and mushrooms and cook for 3-5 minutes, then push to the side with beef.</li>
-<li>Stir in flour into the juices on empty side of pan.  Pour in broth and bring to a boil, stirring constantly.  Lower the heat and stir in mustard.  Cover and simmer for ~ 1/2 hour.</li>
+<li>Stir in flour into the juices on empty side of pan.  Pour in broth and bring to a boil, stirring constantly.  Lower the heat and stir in mustard.  Cover and simmer for 20 minutes.</li>
 <li>Five minutes before serving, stir in sour cream and white wine.  Heat briefly then serve over noodles.  Serves ~ 4.</li>
 </ol>
 

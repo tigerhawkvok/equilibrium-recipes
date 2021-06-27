ed752d17091d8c414d8eedffd833a9a034aed78c
Philip Kahn <tigerhawkvok@gmail.com>
2021-06-26 19:13:43 -0700

    I always use chicken broth....

--- beef_stroganoff.php
+++ beef_stroganoff.php
@@ -9,9 +9,9 @@ include('header.inc');
 <li>1/2 teaspoon pepper</li>
 <li>4 oz. butter</li>
 <li>6 green onions (chopped)</li>
-<li>4 tablespoons flour (heaping)</li>
-<li>2 cups beef broth</li>
-<li>~ 1 teaspoon mustard</li>
+<li>4 tablespoons flour or cornstarch</li>
+<li>2 cups chicken broth</li>
+<li>~ 1/2 Tbsp mustard</li>
 <li>1.5 cups mushrooms (sliced)</li>
 <li>1/2 cup sour cream (full, or slightly more)</li>
 <li>~ 1/3 cup white wine</li>
@@ -20,7 +20,7 @@ include('header.inc');
 <ol>
 <li>Cut beef into strips and season with salt and pepper</li>
 <li>In skillet, melt the butter and brown the beef.  Push them off to one side.  Add onions and mushrooms and cook for 3-5 minutes, then push to the side with beef.</li>
-<li>Stir in flour into the juices on empty side of pan.  Pour in beef broth and bring to a boil, stirring constantly.  Lower the heat and stir in mustard.  Cover and simmer for ~ 1/2 hour.</li>
+<li>Stir in flour into the juices on empty side of pan.  Pour in broth and bring to a boil, stirring constantly.  Lower the heat and stir in mustard.  Cover and simmer for ~ 1/2 hour.</li>
 <li>Five minutes before serving, stir in sour cream and white wine.  Heat briefly then serve over noodles.  Serves ~ 4.</li>
 </ol>
 
